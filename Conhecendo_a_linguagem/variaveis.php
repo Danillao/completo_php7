90e11c991e816933a1275e3bae216927e1139543
Danillao <joufilho@gmail.com>
2020-11-28 17:38:14 -0300

    editei o arquivo variaveis.php

---
+++ Conhecendo_a_linguagem/variaveis.php
@@ -0,0 +1,8 @@
+<?php
+
+// A declaração das variaveis se inicia com o $ ;
+// Nao é necessário declarar o tipo da variavel em php, no caso ela 
+// é uma linguagem fracamente tipada ;
+
+$nome = "HCODE";
+$idade = 40;
